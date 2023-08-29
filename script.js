439df74566425d9757b78a7f7c1a0c88515d8816
Pavan Navde <136722639+pavannavde@users.noreply.github.com>
2023-08-29 10:35:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,28 @@
-window.promises = [];
+window.promises = [new Promise((resolve)=>{
+		setTimeout(()=>{
+			resolve(`Hello Pavan !`)
+		},1000)
+	}),
+	 new Promise((resolve)=>{
+		setTimeout(()=>{
+			resolve(`hello pavan1 !`)
+		},2000)
+	}),
+   new Promise((resolve)=>{
+		setTimeout(()=>{
+			resolve(`hello pavan2 !`)
+		},3000)
+	}),
+	 new Promise((resolve)=>{
+		setTimeout(()=>{
+			resolve(`hello pavan 3!`)
+		},4000)
+	}),
+	 new Promise((reject)=>{
+		setTimeout(()=>{
+			reject(`hello pavan 4!`)
+		},500)
+	})];
 
 // Do not change the code above this
 // add your promises to the array `promises`

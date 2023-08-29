0afaf9c6c6d585e29bc114e1fbaddb9da89b8289
Pavan Navde <136722639+pavannavde@users.noreply.github.com>
2023-08-29 10:23:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,33 +3,33 @@ window.promises = [];
 // Do not change the code above this
 // add your promises to the array `promises`
  let output=document.getElementById("output");
-let result = Promise.any(()=>{
-	const promise1 = new Promise((resolve)=>{
+let result = Promise.any([
+	 new Promise((resolve)=>{
 		setTimeout(()=>{
 			resolve(`Hello Pavan !`)
 		},1000)
-	})
-	const promise2 = new Promise((resolve)=>{
+	}),
+	 new Promise((resolve)=>{
 		setTimeout(()=>{
-			resolve(`hello pavan !`)
+			resolve(`hello pavan1 !`)
 		},2000)
-	})
-		const promise3 = new Promise((resolve)=>{
+	}),
+   new Promise((resolve)=>{
 		setTimeout(()=>{
-			resolve(`hello pavan !`)
+			resolve(`hello pavan2 !`)
 		},3000)
-	})
-		const promise4 = new Promise((resolve)=>{
+	}),
+	 new Promise((resolve)=>{
 		setTimeout(()=>{
-			resolve(`hello pavan !`)
+			resolve(`hello pavan 3!`)
 		},4000)
-	})
-	const promise5 = new Promise((resolve)=>{
+	}),
+	 new Promise((reject)=>{
 		setTimeout(()=>{
-			resolve(`hello pavan !`)
-		},5000)
+			reject(`hello pavan 4!`)
+		},500)
 	})
-})
+])
 result.then((data)=>{
 	output.innerText=data;
 })
